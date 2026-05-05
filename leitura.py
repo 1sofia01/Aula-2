dd3c45f57caead0142aadb2abfe5df0f1831fb0a
1sofia01 <sofiaaah88@gmail.com>
2026-05-05 15:17:55 -0300

    Correct dataframe display for party and state input
    Fix dataframe display logic based on user input.

--- leitura.py
+++ leitura.py
@@ -10,3 +10,10 @@ st.write(df.columns)
 sigla = st.text_input('Digite a sigla do partido')
 if sigla:
   st.dataframe(df[df['partido']==sigla.upper()])
+else:
+  st.dataframe = df
+
+if uf:
+  st.dataframe = df_filtrado[df_filtrado['uf']==uf.upper()]
+
+st.dataframe(df_filtrado)
